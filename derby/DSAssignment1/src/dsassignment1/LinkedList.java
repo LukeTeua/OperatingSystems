0fe8e94f1036bd32682a656efedc4a5885b03c88
lukey <lukey@lukey-IdeaPad-3-14IML05>
2022-07-31 19:42:42 +1200

    Pushing DSA Assignment One

---
+++ derby/DSAssignment1/src/dsassignment1/LinkedList.java
@@ -0,0 +1,78 @@
+package dsassignment1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lukey
+ */
+public class LinkedList <E>{
+
+Node head;
+E Data;
+//stores the number of nodes in the LinkedList
+
+//An arrayList of nodes
+ArrayList <Node> List;
+
+
+//returns the size of the arrayList
+int size;
+
+
+public LinkedList(){
+
+
+
+
+
+
+}
+
+
+
+
+public void add(E data){
+
+//add some data here
+    
+}
+public void addInOrder(E data){
+
+
+
+
+
+}
+public boolean contains(Node node){
+
+
+return true; 
+
+}
+
+
+public void remove(Node node){
+//remove node from list
+//pass a node into this.
+
+
+
+}
+
+
+public void removeFromHead(Node node){
+
+//remove the node from the head of the linked list
+
+
+}
+
+
+public void removeFromTail(Node node ){
+
+// removes node from the tail of the linked list. 
+
+}
+    
+}
