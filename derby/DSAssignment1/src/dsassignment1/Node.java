1c2ca62bd2cf9172d4e47f3d9666e61f630d1a65
lukey <lukey@lukey-IdeaPad-3-14IML05>
2022-07-31 19:46:32 +1200

    Pushing DSA Assignment One

--- derby/DSAssignment1/src/dsassignment1/Node.java
+++ derby/DSAssignment1/src/dsassignment1/Node.java
@@ -44,7 +44,6 @@ public class Node<E> {
         }
 
     }
-    
 
     public static void main(String[] args) {
         Node<Integer> node1 = new Node(3, null);
@@ -54,29 +53,27 @@ public class Node<E> {
         Node<Integer> node5 = new Node(8, node4);
 
         //this gets the second item in the linked list. 
-        System.out.println("Printing Node 1:"+ node1.getData());
-        System.out.println("Printing Node 2:"+ node2.getData());
-        System.out.println("Printing Node 3:" +node3.getData());
-        System.out.println("Printing Node 4:"+node4.getData());
-        System.out.println("Printing Node 5:"+node5.getData());
+        System.out.println("Printing Node 1:" + node1.getData());
+        System.out.println("Printing Node 2:" + node2.getData());
+        System.out.println("Printing Node 3:" + node3.getData());
+        System.out.println("Printing Node 4:" + node4.getData());
+        System.out.println("Printing Node 5:" + node5.getData());
         System.out.println("Node 1 equals node 2 should return true:" + node1.getData().equals(node2.getData()));
         System.out.println("Comparing node 1 to node 2 return 0 if the node values are using equals method equal:" + node1.getData().compareTo(node2.getData()));
         System.out.println("Node 1 comparing to node 2 should return 0:" + node2.getData().compareTo(node1.getData()));
-        System.out.println("Since the value at node 3 is larger than node 5 should return a positive number:" +node3.getData().compareTo(node5.getData()));
-        System.out.println("Since node 1 is smaller than node 4 the number returned should be negative:"+node1.getData().compareTo(node4.getData()));
-    
-    
-        Node<String> stringNode1 = new Node("A" ,null);
-        Node<String> stringNode2 = new Node("A" ,null);
-        Node<String> stringNode3 = new Node("B" ,null);
-        System.out.println("Printing String node:" +stringNode1.getData());
-        System.out.println("Printing String node:" +stringNode2.getData());
-        System.out.println("Printing String node:" +stringNode3.getData());
+        System.out.println("Since the value at node 3 is larger than node 5 should return a positive number:" + node3.getData().compareTo(node5.getData()));
+        System.out.println("Since node 1 is smaller than node 4 the number returned should be negative:" + node1.getData().compareTo(node4.getData()));
+
+        Node<String> stringNode1 = new Node("A", null);
+        Node<String> stringNode2 = new Node("A", null);
+        Node<String> stringNode3 = new Node("B", null);
+        System.out.println("Printing String node:" + stringNode1.getData());
+        System.out.println("Printing String node:" + stringNode2.getData());
+        System.out.println("Printing String node:" + stringNode3.getData());
         System.out.println("StringNode1 equals node 2 should return false ,integer vs String:" + stringNode3.getData().equals(node4.getData()));
-        System.out.println("Comparing the two stringnodes stringNode1 and stringNode2:" +stringNode1.getData().compareTo(stringNode2.getData()));
-        System.out.println("Equals method on stringNode1 and stringNode3:" +stringNode1.getData().compareTo(stringNode3.getData()));
-        
-        
+        System.out.println("Comparing the two stringnodes stringNode1 and stringNode2:" + stringNode1.getData().compareTo(stringNode2.getData()));
+        System.out.println("Equals method on stringNode1 and stringNode3:" + stringNode1.getData().compareTo(stringNode3.getData()));
+
     }
 
 }
